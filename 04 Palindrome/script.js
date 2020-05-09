3fac78218d3c283255af707b866f2a4e3225e5b1
Mohammad Mazharul Hoque <mhoque@ael-bd.com>
2020-05-09 23:58:24 +0600

    problem solving with js- 04. Palindrome

---
+++ 04 Palindrome/script.js
@@ -0,0 +1,26 @@
+/**Given a word, return  true if it is a palindrome, 
+ **otherwise return false
+A palindrome is a word which reads the same backward as forward
+e.g. racecar.**/
+// --Examples 
+// palindrome('orange') === 'egnaro'
+// palindrome('racecar') === 'racecar'
+
+//solution
+let isPalindrome = str=>{
+    return str.split('').every((element, index)=>{
+        return element === str[str.length - index -1];
+    });
+}
+console.log(isPalindrome('racecar'));
+
+// another solution
+let isPalindromeTwo = str=>{
+    const reversedStr = str
+        .split('')
+        .reverse()
+        .join('');
+        return str === reversedStr;
+}
+console.log(isPalindromeTwo('orange'))
+
